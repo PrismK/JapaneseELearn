cfbbd18f0793bcd9eeea076d878d875fb5cf0acc
prismk <18340811265@163.com>
2019-03-24 12:05:59 +0800

    [Database]Add database tables

---
+++ app/src/main/java/com/prismk/japaneseelearn/managers/TeacherFollowedDBManager.java
@@ -0,0 +1,73 @@
+package com.prismk.japaneseelearn.managers;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.prismk.japaneseelearn.bean.TeacherFollowedData;
+import com.prismk.japaneseelearn.bean.UserData;
+
+public class TeacherFollowedDBManager {
+
+    private static final String DB_NAME = "e_learn_db";
+    private static final String TABLE_NAME = "teacher_followed";
+    public static final String ID = "_id";
+    public static final String STUDENT_ID = "student_id";
+    public static final String TEACHER_ID = "teacher_id";
+
+    private static final int DB_VERSION = 1;
+
+    private Context mContext;
+
+    private static final String DB_CREATE = "CREATE TABLE " + TABLE_NAME + " ("
+            + ID + " integer primary key," + STUDENT_ID + " integer,"
+            + TEACHER_ID + " inteager"
+            + ");";
+
+    private SQLiteDatabase mSQLiteDatabase = null;
+    private DataBaseManagementHelper mDatabaseHelper = null;
+
+    private static class DataBaseManagementHelper extends SQLiteOpenHelper {
+
+        DataBaseManagementHelper(Context context) {
+            super(context, DB_NAME, null, DB_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME + ";");
+            db.execSQL(DB_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onCreate(db);
+        }
+    }
+
+    public TeacherFollowedDBManager(Context context) {
+        mContext = context;
+    }
+
+    public void openDataBase() throws SQLException {
+        mDatabaseHelper = new DataBaseManagementHelper(mContext);
+        mSQLiteDatabase = mDatabaseHelper.getWritableDatabase();
+    }
+
+    public void closeDataBase() throws SQLException {
+        mDatabaseHelper.close();
+    }
+
+    public long insertFollowData(TeacherFollowedData teacherFollowedData) {
+        int studentId = teacherFollowedData.getStudentId();
+        int teacherId = teacherFollowedData.getTeacherId();
+        ContentValues values = new ContentValues();
+        values.put(STUDENT_ID, studentId);
+        values.put(TEACHER_ID, teacherId);
+        return mSQLiteDatabase.insert(TABLE_NAME, ID, values);
+    }
+
+}
