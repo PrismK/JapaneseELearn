67040e49a73146b4f4407baf373d267547f45244
prismk <18340811265@163.com>
2019-04-02 11:39:09 +0800

    更改假数据，添加头像URL

--- app/src/main/java/com/prismk/japaneseelearn/managers/UserDBManager.java
+++ app/src/main/java/com/prismk/japaneseelearn/managers/UserDBManager.java
@@ -61,7 +61,7 @@ public class UserDBManager {
                 values.put(USER_NICKNAME, "" + i + "号学生的昵称");
                 values.put(USER_SIGN, "" + i + "号学生的个性签名");
                 values.put(USER_TAG, "" + i + "号学生的标签");
-                values.put(USER_HEADIMG, "");
+                values.put(USER_HEADIMG, "http://111.231.206.89/lyf/image/1.png");
                 values.put(ISTEACHER, 0);
                 if (i >= 6) {
                     values.put(ISVIP, 1);
